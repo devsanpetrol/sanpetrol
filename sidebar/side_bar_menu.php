be0a53d14ae674f36d00e3397c24a9f2d6bd8f0c
user <user@192.168.1.135>
2019-08-23 08:00:35 -0500

    pagina inicial plantilla

---
+++ sidebar/side_bar_menu.php
@@ -0,0 +1,31 @@
+<!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu abajo">
+              <div class="menu_section">
+                <h3>Inicio</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Suministro <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Solicitud <span class="label label-danger pull-right">Construcción</span></a></li>
+                    </ul>
+                  </li>
+                  
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Live On</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="e_commerce.html">E-commerce</a></li>
+                      <li><a href="projects.html">Projects</a></li>
+                      <li><a href="project_detail.html">Project Detail</a></li>
+                      <li><a href="contacts.html">Contacts</a></li>
+                      <li><a href="profile.html">Profile</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page </a></li>
+                </ul>
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
